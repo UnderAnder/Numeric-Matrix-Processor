737413d357c9be4e82e8cda7e62e74bea9fcad5b
UnderAnder <andryushin.alexander@gmail.com>
2021-04-17 21:14:02 +0700

    Stage 2/6: Multiplication by number

--- processor.py
+++ processor.py
@@ -1,34 +1,38 @@
-def input_matrix():
-    matrix = list()
-    rows, cols = map(int, input().split())
-    for row in range(rows):
-        matrix.append(list(map(int, input().split())))
-    return matrix
+class MatrixDimensionError(Exception):
+    pass
 
 
-def matrix_sum(a: list[list[int]], b: list[list[int]]):
-    if len(a) != len(b):
-        return None
-    matrix = list()
-    for row_a, row_b in zip(a, b):
-        row = list()
-        for el_a, el_b in zip(row_a, row_b):
-            row.append(el_a+el_b)
-        matrix.append(row)
-    return matrix
+class Matrix:
+    def __init__(self, matrix=None):
+        self.matrix = matrix if matrix else self.__input_matrix()
+        self.rows = len(self.matrix)
+        self.cols = len(self.matrix[0])
 
+    def __add__(self, other):
+        if self.rows != other.rows or self.cols != other.cols:
+            raise MatrixDimensionError
+        else:
+            result_matrix = list(map(lambda x, y: list(map(lambda i, j: i + j, x, y)), self.matrix, other.matrix))
+            return Matrix(result_matrix)
 
-def print_matrix(matrix: list[list[int]]):
-    if not matrix:
-        print('ERROR')
-        return False
-    for i in matrix:
-        print(' '.join(map(str, i)))
+    def __mul__(self, other):
+        return Matrix([[other * j for j in i] for i in self.matrix])
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __str__(self):
+        return '\n'.join([' '.join(map(str, i)) for i in self.matrix])
+
+    def __input_matrix(self):
+        rows, cols = map(int, input().split())
+        return [[int(i) for i in input().split()] for _ in range(rows)]
 
 
 def main():
-    print_matrix(matrix_sum(input_matrix(), input_matrix()))
-
+    matrix = Matrix()
+    scalar = int(input())
+    print(matrix * scalar)
 
 if __name__ == '__main__':
     main()
